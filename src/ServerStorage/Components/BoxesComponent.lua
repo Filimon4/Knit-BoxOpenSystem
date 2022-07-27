40a7b5ec779200e2b73f0c2662da5147b0e39383
FILIMON <f.rykov@bk.ru>
2022-07-27 22:55:19 +0300

    Add Component

--- src/ServerStorage/Components/BoxesComponent.lua
+++ src/ServerStorage/Components/BoxesComponent.lua
@@ -1,6 +1,47 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local Component = require(ReplicatedStorage.Packages.Component)
+local Signal = require(ReplicatedStorage.Packages.Signal)
 
-local BosexComponent = {}
+local BoxesFolder = game.Workspace:WaitForChild('Boxes')
 
-return BosexComponent
+local BoxesInfo = require(script.Parent.Parent.Modules.BoxesInfo)
+
+local Loger = {}
+function Loger.ShouldConstruct(component)
+    if not Component.Instance:IsA('Model') then return false end
+    if Component.Instance.Parent ~= BoxesFolder then return false end
+    return true
+end
+
+local BoxesComponent = Component.new{
+    Tag = BoxesInfo.TAG,
+    Ancestors = {BoxesFolder},
+    Extensions = Loger
+}
+
+function BoxesComponent:onTouched(hit)
+    local plr = Players:GetPlayerFromCharacter(hit.Parent)
+    if plr then
+        print(plr.Name)
+    end
+end
+
+function BoxesComponent:Construct()
+    print('Constructing')
+    self.Win = math.random(50, 1000)
+end
+
+function BoxesComponent:Start()
+    self.Instance.PrimaryPart.Touched:Connect(function(hit)
+        self:onTouched(hit)
+    end)
+end
+
+function BoxesComponent:Stop()
+    print('Stop')
+end
+
+return BoxesComponent
