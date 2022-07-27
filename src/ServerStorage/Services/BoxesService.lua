06d471fff3cef2d2ed0c27f2df8373ae254834b5
FILIMON <f.rykov@bk.ru>
2022-07-27 20:37:16 +0300

    add: Component, Signal, Trove

---
+++ src/ServerStorage/Services/BoxesService.lua
@@ -0,0 +1,28 @@
+local LocalizationService = game:GetService("LocalizationService")
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+local CollectionService = game:GetService("CollectionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local BoxesFolder = game.Workspace:WaitForChild('Boxes')
+local Box = ReplicatedStorage.Props:WaitForChild("Box")
+local MaxBoxes = 25
+
+local GameStart = Knit.CreateService{
+    Name = "GameStart",
+    Client = {}
+}
+
+function GameStart:Create()
+    for i=0,MaxBoxes do
+        local Box_Clone = Box:Clone()
+        Box_Clone.Parent = BoxesFolder
+        Box_Clone:MoveTo(Vector3.new(math.random(-210,230), game.Workspace.Baseplate.Position.Y + 2, math.random(-210,230)))
+        CollectionService:AddTag(Box_Clone, "Box")
+    end
+end
+
+function GameStart:KnitStart()
+    self:Create()
+end
+
+return GameStart
